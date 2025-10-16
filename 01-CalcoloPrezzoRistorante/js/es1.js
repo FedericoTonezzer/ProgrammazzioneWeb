ee47715466b98421118c724308c1e9652cd9e34f
tambosi\federico.tonezzer <federico>
2025-10-16 12:11:44 +0200

    es 1 quasi finito

---
+++ 01-CalcoloPrezzoRistorante/js/es1.js
@@ -0,0 +1,73 @@
+"use strict";
+
+function btnCalcola_Click() {
+  let qPrimi;
+  let qSecondi;
+  let totPrimi;
+  let totSecondi;
+  let seSalaRiservata;
+  let importoSalaRiservata;
+  let iva;
+  let imponibile;
+  let totale;
+
+  let txtPrimi = document.getElementById("txtPrimi");
+  let txtSecondi = document.getElementById("txtSecondi");
+  let chkSalaRiservata = document.getElementById("chkSalaRiservata");
+  let lblTotale = document.getElementById("lblTotale");
+
+  if (checkData(txtPrimi, txtSecondi) == true) {
+    // calcolo totale
+    qPrimi = Number(txtPrimi.value);
+    qSecondi = Number(txtSecondi.value);
+
+    totPrimi = qPrimi * 9;
+    totSecondi = qSecondi * 12;
+
+    seSalaRiservata = chkSalaRiservata.checked;
+
+    if (seSalaRiservata == true) {
+      importoSalaRiservata = 10;
+    } else {
+      importoSalaRiservata = 0;
+    }
+
+    imponibile = totPrimi + totSecondi + importoSalaRiservata;
+
+    iva = calcolaPercentuale(imponibile, 22);
+
+    totale = imponibile + iva;
+
+    lblTotale.textContent = totale.toFixed(2) + "EUR";
+  } else {
+    //qualcosa di sbagliato
+    alert("I dati in input non sono corretti");
+  }
+}
+
+function calcolaPercentuale(value, perc) {
+  return (value * perc) / 100;
+}
+
+// controllo dati obbligatori
+function checkData(txtPrimi, txtSecondi) {
+  let valido;
+  // --> && AND - || --> OR
+  if (txtPrimi.vale == "" || txtSecondi == "") {
+    valido = false;
+  } else {
+    valido = true;
+  }
+  return valido;
+}
+
+function validateNumberField(txtNumberTextBox) {
+  let errorClass = "error";
+
+  let isValida = txtNumberTextBox.checkValidity(); //controllo la validità
+  if (!isValida) {
+    txtNumberTextBox.classList.add(errorClass); //se non valida agg error
+  } else {
+    txtNumberTextBox.classList.remove(errorClass); //se valida rimuovo error
+  }
+}
