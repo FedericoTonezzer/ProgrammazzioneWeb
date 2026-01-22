a7f91a5cc8356cc2f3a68a0dae4b8d6892f809e0
tambosi\federico.tonezzer <federico>
2026-01-22 12:13:46 +0100

    es 9 corretto

--- 09 - Persone/js/classes/persona.js
+++ 09 - Persone/js/classes/persona.js
@@ -1,62 +1,58 @@
 "use strict";
 
 class Persona {
-    #codice_fiscale
-    #nome;
-    #cognome;
+  #codice_fiscale;
+  #nome;
+  #cognome;
 
-    constructor(valCodiceFiscale, valNome, valCognome) {
-      this.codice_fiscale = valCodiceFiscale;
-      this.nome = valNome;
-      this.cognome = valCognome;
-    }
+  constructor(valCodiceFiscale, valNome, valCognome) {
+    this.codice_fiscale = valCodiceFiscale;
+    this.nome = valNome;
+    this.cognome = valCognome;
+  }
 
-    get codice_fiscale() {
-        return this.#codice_fiscale;
-    }
+  get codice_fiscale() {
+    return this.#codice_fiscale;
+  }
 
-    set codice_fiscale(valore) {
-        if(this.validateCodiceFiscale(valore)){
-            this.#codice_fiscale = valore;
-        } else{
-            throw new Error("Il codice fisclae non è valido")
-        }
+  set codice_fiscale(valore) {
+    if (this.validateCodiceFiscale(valore)) {
+      this.#codice_fiscale = valore;
+    } else {
+      throw new Error("Il codice fisclae non è valido");
     }
+  }
 
-    get nome_cognome() {
-        return this.nome + " " + this.cognome;
-    }
+  get nome_cognome() {
+    return this.nome + " " + this.cognome;
+  }
 
-    get nome() {
-        return this.#nome;
-    }
+  get nome() {
+    return this.#nome;
+  }
 
-    set nome(valore) {
-        if(!this.isNullOrEmpty(valore)){
-            this.#nome = valore;
-        } else{
-            throw new Error("Il nome è obbligatorio")
-        }
+  set nome(valore) {
+    if (!this.isNullOrEmpty(valore)) {
+      this.#nome = valore;
+    } else {
+      throw new Error("Il nome è obbligatorio");
     }
+  }
 
-    get cognome() {
-        return this.#cognome;
-    }
+  get cognome() {
+    return this.#cognome;
+  }
 
-    set cognome(valore) {
-        if(!this.isNullOrEmpty(valore)){
-            this.#cognome = valore;
-        } else{
-            throw new Error("Il cognome è obbligatorio")
-        }
+  set cognome(valore) {
+    if (!this.isNullOrEmpty(valore)) {
+      this.#cognome = valore;
+    } else {
+      throw new Error("Il cognome è obbligatorio");
     }
+  }
 
-    set cognome(valore){
-        if (!this.is)
-    }
-
-    validateCodiceFiscale(codice_fiscale) {
-        let valido = codice_fiscale != null && codice_fiscale.lenght == 16;
-        return valido; 
-    }
+  validateCodiceFiscale(codice_fiscale) {
+    let valido = codice_fiscale != null && codice_fiscale.lenght == 16;
+    return valido;
+  }
 }
